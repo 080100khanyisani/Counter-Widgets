dc332bec78a9ddaa13864096f213a99f128ae5e3
080100khanyisani <khanyisani1010@gmail.com>
2024-07-11 09:47:12 +0200

    fourth commit

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 let counter = 0;
 const maxCounterValue = 10;
 
-document.addEventListener("DOMContentLoaded", function() {
-    const counterElem = document.querySelector('.counter');
-    const incrementBtn = document.querySelector('.increment');
-    const decrementBtn = document.querySelector('.decrement');
-    const resetBtn = document.querySelector('.reset-js');
-    const caption = document.querySelector('.caption');
+const counterElem = document.querySelector('.counter');
+const incrementBtn = document.querySelector('.increment');
+const decrementBtn = document.querySelector('.decrement');
+const resetBtn = document.querySelector('.reset-js');
 
-    caption.innerText = 'Counter';
-
-    incrementBtn.addEventListener('click', function() {
-        counter++;
-        if (counter > maxCounterValue) {
-            counter = 0;
-        }
-        counterElem.innerText = counter;
-    });
-
-    decrementBtn.addEventListener('click', function() {
-        counter = Math.max(0, counter - 1);
-        counterElem.innerText = counter;
-    });
-
-    resetBtn.addEventListener('click', function() {
+incrementBtn.addEventListener('click', function() {
+    counter++;
+    if (counter > maxCounterValue) {
         counter = 0;
-        counterElem.innerText = counter;
-    });
+    }
+    counterElem.innerText = counter;
 });
+
+decrementBtn.addEventListener('click', function() {
+    if (counter > 0) {
+        counter--;
+    }
+    counterElem.innerText = counter;
+});
+
+resetBtn.addEventListener('click', function() {
+    counter = 0;
+    counterElem.innerText = counter;
+});
+
+const caption = document.querySelector('.caption');
+caption.innerText = 'Counter';
+console.log(caption);
